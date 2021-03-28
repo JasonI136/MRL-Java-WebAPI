4d723c5a98b7dac0bbfde9c0dedcefb1622580d3
JasonI136 <63634201+JasonI136@users.noreply.github.com>
2021-03-28 21:59:39 +1100

    Calculator v1

--- src/main/java/com/jason/demo/controller/HelloController.java
+++ src/main/java/com/jason/demo/controller/HelloController.java
@@ -1,13 +1,33 @@
 package com.jason.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hw")
-
-    public String getHelloWorld(){
-        return "Hello World";
+    @RequestMapping("/add")
+    public int Add(@RequestParam(value = "n1", defaultValue = "0") int n1, @RequestParam(value = "n2", defaultValue = "0")  int n2)
+    {
+        return n1 + n2;
     }
+
+    @RequestMapping("/subtract")
+    public int Subtract(@RequestParam(value = "n1", defaultValue = "0") int n1, @RequestParam(value = "n2", defaultValue = "0")  int n2)
+    {
+        return n1 - n2;
+    }
+
+    @RequestMapping("/multiply")
+    public int Multiply(@RequestParam(value = "n1", defaultValue = "0") int n1, @RequestParam(value = "n2", defaultValue = "0")  int n2)
+    {
+        return n1 * n2;
+    }
+
+    @RequestMapping("/divide")
+    public float Divide(@RequestParam(value = "n1", defaultValue = "0") int n1, @RequestParam(value = "n2", defaultValue = "0")  int n2)
+    {
+        return n1 / n2;
+    }
+
 }
